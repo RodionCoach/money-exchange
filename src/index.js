bed0a96b0d08231ce465ec250a2c23e2705e3959
Rodion Kochetkov <www.radik@yandex.by>
2019-03-24 23:59:50 +0300

    solution: not all completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let obj = {};
+    while(currency > 1){
+        if(currency % 50 > 0){
+            obj['H']++;
+            currency - 50;
+        }else if(currency % 25 > 0){
+            obj['Q']++;
+            currency - 25;
+        }else if(currency % 10 > 0){
+            obj['D']++;
+            currency - 10;
+        }else if(currency % 5 > 0){
+            obj['N']++;
+            currency - 5;
+        }else if(currency % 1 > 0){
+            obj['P']++;
+            currency - 1;
+        }
+    }
+
 }
